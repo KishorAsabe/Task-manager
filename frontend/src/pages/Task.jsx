98db62782607a0218cc249f0eed675cbd5e46f33
KishorAsabe <kishorasabe2020@gmail.com>
2024-01-24 09:36:09 +0530

    update UI of Task.jsx

--- frontend/src/pages/Task.jsx
+++ frontend/src/pages/Task.jsx
@@ -86,26 +86,35 @@ const Task = () => {
 
   return (
     <>
-      <MainLayout>
-        <form className='m-auto my-16 max-w-[1000px] bg-white p-8 border-2 shadow-md rounded-md'>
-          {loading ? (
-            <Loader />
-          ) : (
-            <>
-              <h2 className='text-center mb-4'>{mode === "add" ? "Add New Task" : "Edit Task"}</h2>
-              <div className="mb-4">
-                <label htmlFor="description">Description</label>
-                <Textarea type="description" name="description" id="description" value={formData.description} placeholder="Write here.." onChange={handleChange} />
-                {fieldError("description")}
-              </div>
+<MainLayout>
+  <form className='m-auto my-16 max-w-[1000px] bg-richblack-800 p-8 border-0  shadow-md rounded-md text-richblack-5 '>
+    {loading ? (
+      <Loader />
+    ) : (
+      <>
+        <h2 className='text-center mb-4 font-bold'>{mode === "add" ? "Add New Task" : "Edit Task"}</h2>
+        <div className="mb-4">
+          <label htmlFor="description" className="text-richblack-5">Description</label>
+          <Textarea type="description" name="description" id="description" value={formData.description} placeholder="Write here.." onChange={handleChange} className="w-full rounded-md bg-richblack-700 p-2 text-richblack-5" />
+          {fieldError("description")}
+        </div>
 
-              <button className='bg-primary text-white px-4 py-2 font-medium hover:bg-primary-dark' onClick={handleSubmit}>{mode === "add" ? "Add task" : "Update Task"}</button>
-              <button className='ml-4 bg-red-500 text-white px-4 py-2 font-medium' onClick={() => navigate("/")}>Cancel</button>
-              {mode === "update" && <button className='ml-4 bg-blue-500 text-white px-4 py-2 font-medium hover:bg-blue-600' onClick={handleReset}>Reset</button>}
-            </>
-          )}
-        </form>
-      </MainLayout>
+        <button className='bg-yellow-50 text-richblack-900 px-4 py-2 font-medium hover:bg-yellow-100 rounded-md transition-all' onClick={handleSubmit}>
+          {mode === "add" ? "Add task" : "Update Task"}
+        </button>
+        <button className='ml-4 bg-red-500 text-white px-4 py-2 font-medium hover:bg-red-600 transition-all rounded-md' onClick={() => navigate("/")}>
+          Cancel
+        </button>
+        {mode === "update" && 
+          <button className='ml-4 bg-blue-500 text-white px-4 py-2 font-medium hover:bg-blue-600 transition-all' onClick={handleReset}>
+            Reset
+          </button>
+        }
+      </>
+    )}
+  </form>
+</MainLayout>
+
     </>
   )
 }
