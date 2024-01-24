ee85c412eed848f9d1ad19e4fddc3affafc259f7
KishorAsabe <kishorasabe2020@gmail.com>
2024-01-24 09:37:23 +0530

    update buttons Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,63 +1,117 @@
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { logout } from '../redux/actions/authActions';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { logout } from "../redux/actions/authActions";
+import logo from "../assets/task.jpg";
 
 const Navbar = () => {
-
-  const authState = useSelector(state => state.authReducer);
+  const authState = useSelector((state) => state.authReducer);
   const dispatch = useDispatch();
   const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+
   const toggleNavbar = () => {
     setIsNavbarOpen(!isNavbarOpen);
-  }
+  };
 
   const handleLogoutClick = () => {
     dispatch(logout());
-  }
+  };
 
   return (
     <>
-      <header className='flex justify-between sticky top-0 p-4 bg-white shadow-sm items-center'>
-        <h2 className='cursor-pointer uppercase font-medium'>
-          <Link to="/"> Task Manager </Link>
-        </h2>
-        <ul className='hidden md:flex gap-4 uppercase font-medium'>
+      <header className="flex  justify-between  top-0 p-4 items-center font-inter border-b-[1px] border-b-richblack-700">
+        <div className="flex items-center  w-11/12 max-w-maxContent ">
+          <h2 className="cursor-pointer text-caribbeangreen-300 uppercase font-extrabold text-2xl">
+            <Link to="/" className="flex items-center gap-2">
+              <span className="text-white">Task</span>
+              <span className="text-yellow-300">Manager</span>
+              <i className="fa-solid fa-star text-yellow-300"></i>
+            </Link>
+          </h2>
+        </div>
+        <ul className="hidden md:flex gap-4 uppercase item-center justify-center font-medium">
           {authState.isLoggedIn ? (
             <>
-              <li className="bg-blue-500 text-white hover:bg-blue-600 font-medium rounded-md">
-                <Link to='/tasks/add' className='block w-full h-full px-4 py-2'> <i className="fa-solid fa-plus"></i> Add task </Link>
+              <li className="bg-richblack-900 text-white item-center font-medium rounded-md ">
+                <Link to="/tasks/add" className="block w-full h-full ">
+                  <button
+                    type="button"
+                    className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100 hover:bg-richblack-900 transition-all"
+                  >
+                    <i className="fa-solid fa-plus"></i> Add task
+                  </button>
+                </Link>
               </li>
-              <li className='py-2 px-3 cursor-pointer hover:bg-gray-200 transition rounded-sm' onClick={handleLogoutClick}>Logout</li>
+
+              <li
+                className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100 hover:bg-richblack-900 transition-all"
+                onClick={handleLogoutClick}
+              >
+                Logout
+              </li>
             </>
           ) : (
-            <li className='py-2 px-3 cursor-pointer text-primary hover:bg-gray-100 transition rounded-sm'><Link to="/login">Login</Link></li>
+            <Link to="/login">
+              <button className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100 hover:bg-richblack-900 transition-all">
+                Log in
+              </button>
+            </Link>
           )}
         </ul>
-        <span className='md:hidden cursor-pointer' onClick={toggleNavbar}><i className="fa-solid fa-bars"></i></span>
 
+        <span
+          className="md:hidden text-richblack-100 cursor-pointer"
+          onClick={toggleNavbar}
+        >
+          <i
+            className={`fa-solid ${isNavbarOpen ? "fa-xmark" : "fa-bars"}`}
+          ></i>
+        </span>
 
         {/* Navbar displayed as sidebar on smaller screens */}
-        <div className={`absolute md:hidden right-0 top-0 bottom-0 transition ${(isNavbarOpen === true) ? 'translate-x-0' : 'translate-x-full'} bg-gray-100 shadow-md w-screen sm:w-9/12 h-screen`}>
-          <div className='flex'>
-            <span className='m-4 ml-auto cursor-pointer' onClick={toggleNavbar}><i className="fa-solid fa-xmark"></i></span>
+        <div
+          className={`absolute md:hidden right-0 top-0 bottom-0 transition ${
+            isNavbarOpen === true ? "translate-x-0" : "translate-x-full"
+          } bg-richblack-800 shadow-md w-screen sm:w-9/12 h-screen`}
+        >
+          <div className="flex">
+            <span
+              className="m-4 ml-auto text-richblack-100 cursor-pointer"
+              onClick={toggleNavbar}
+            >
+              <i className="fa-solid fa-xmark"></i>
+            </span>
           </div>
-          <ul className='flex flex-col gap-4 uppercase font-medium text-center'>
+          <ul className="flex flex-col gap-4 uppercase font-medium text-center py-2 px-[12em]">
             {authState.isLoggedIn ? (
               <>
-                <li className="bg-blue-500 text-white hover:bg-blue-600 font-medium transition py-2 px-3">
-                  <Link to='/tasks/add' className='block w-full h-full'> <i className="fa-solid fa-plus"></i> Add task </Link>
+                <li className=" text-white  font-medium transition object-cover px-[]">
+                  <Link to="/tasks/add" className="block w-full h-full">
+                    <button
+                      type="button"
+                      className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100 hover:bg-richblack-900 transition-all"
+                    >
+                      <i className="fa-solid fa-plus"></i> Add task
+                    </button>
+                  </Link>
                 </li>
-                <li className='py-2 px-3 cursor-pointer hover:bg-gray-200 transition rounded-sm' onClick={handleLogoutClick}>Logout</li>
+                <li
+                  className="rounded-[8px] border  border-richblack-700 px-[12px] py-[8px] text-richblack-100 hover:bg-richblack-900 transition-all"
+                  onClick={handleLogoutClick}
+                >
+                  Logout
+                </li>
               </>
             ) : (
-              <li className='py-2 px-3 cursor-pointer text-primary hover:bg-gray-200 transition rounded-sm'><Link to="/login">Login</Link></li>
+              <li className="py-2 px-[12px] cursor-pointer text-richblack-900 hover:bg-richblack-200 transition rounded-sm">
+                <Link to="/login">Login</Link>
+              </li>
             )}
           </ul>
         </div>
       </header>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
