032b577df709d4db29e51db675293ad33464ef76
KishorAsabe <kishorasabe2020@gmail.com>
2024-01-24 09:38:55 +0530

    update Tasks.jsx and MainLayout.jsx

--- frontend/src/components/Tasks.jsx
+++ frontend/src/components/Tasks.jsx
@@ -1,19 +1,24 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import useFetch from '../hooks/useFetch';
-import Loader from './utils/Loader';
-import Tooltip from './utils/Tooltip';
+import React, { useCallback, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+import Loader from "./utils/Loader";
+import Tooltip from "./utils/Tooltip";
 
 const Tasks = () => {
-
-  const authState = useSelector(state => state.authReducer);
+  const authState = useSelector((state) => state.authReducer);
   const [tasks, setTasks] = useState([]);
   const [fetchData, { loading }] = useFetch();
 
   const fetchTasks = useCallback(() => {
-    const config = { url: "/tasks", method: "get", headers: { Authorization: authState.token } };
-    fetchData(config, { showSuccessToast: false }).then(data => setTasks(data.tasks));
+    const config = {
+      url: "/tasks",
+      method: "get",
+      headers: { Authorization: authState.token },
+    };
+    fetchData(config, { showSuccessToast: false }).then((data) =>
+      setTasks(data.tasks)
+    );
   }, [authState.token, fetchData]);
 
   useEffect(() => {
@@ -21,60 +26,79 @@ const Tasks = () => {
     fetchTasks();
   }, [authState.isLoggedIn, fetchTasks]);
 
-
   const handleDelete = (id) => {
-    const config = { url: `/tasks/${id}`, method: "delete", headers: { Authorization: authState.token } };
+    const config = {
+      url: `/tasks/${id}`,
+      method: "delete",
+      headers: { Authorization: authState.token },
+    };
     fetchData(config).then(() => fetchTasks());
-  }
-
+  };
 
   return (
     <>
       <div className="my-2 mx-auto max-w-[700px] py-4">
-
-        {tasks.length !== 0 && <h2 className='my-2 ml-2 md:ml-0 text-xl'>Your tasks ({tasks.length})</h2>}
+        {tasks.length !== 0 && (
+          <h2 className="my-2 ml-2 md:ml-0 text-xl text-richblack-5">
+            Your tasks ({tasks.length})
+          </h2>
+        )}
         {loading ? (
           <Loader />
         ) : (
-          <div>
+          <div className="">
             {tasks.length === 0 ? (
-
-              <div className='w-[600px] h-[300px] flex items-center justify-center gap-4'>
-                <span>No tasks found</span>
-                <Link to="/tasks/add" className="bg-blue-500 text-white hover:bg-blue-600 font-medium rounded-md px-4 py-2">+ Add new task </Link>
+              <div className="w-[600px] h-[300px] flex items-center justify-center gap-4 ">
+                <h2 className="text-xl text-richblack-5 font-bold mb-4 pt-6">
+                  No tasks found
+                </h2>
+                <Link
+                  to="/tasks/add"
+                  className="bg-yellow-50 text-richblack-900 px-4 py-2 font-medium hover:bg-yellow-100 rounded-md transition-all"
+                >
+                  + Add new task
+                </Link>
               </div>
-
             ) : (
               tasks.map((task, index) => (
-                <div key={task._id} className='bg-white my-4 p-4 text-gray-600 rounded-md shadow-md'>
-                  <div className='flex'>
-
-                    <span className='font-medium'>Task #{index + 1}</span>
+                <div
+                  key={task._id}
+                  className="bg-richblack-700 border-2 my-4 p-4 text-gray-600 rounded-md shadow-md"
+                >
+                  <div className="flex ">
+                    <span className=" text-yellow-25 font-bold ">
+                      Task #{index + 1}
+                    </span>
 
                     <Tooltip text={"Edit this task"} position={"top"}>
-                      <Link to={`/tasks/${task._id}`} className='ml-auto mr-2 text-green-600 cursor-pointer'>
+                      <Link
+                        to={`/tasks/${task._id}`}
+                        className="ml-auto mr-2 text-white cursor-pointer"
+                      >
                         <i className="fa-solid fa-pen"></i>
                       </Link>
                     </Tooltip>
 
                     <Tooltip text={"Delete this task"} position={"top"}>
-                      <span className='text-red-500 cursor-pointer' onClick={() => handleDelete(task._id)}>
+                      <span
+                        className="text-red-600 cursor-pointer "
+                        onClick={() => handleDelete(task._id)}
+                      >
                         <i className="fa-solid fa-trash"></i>
                       </span>
                     </Tooltip>
-
                   </div>
-                  <div className='whitespace-pre'>{task.description}</div>
+                  <div className="whitespace-pre text-white">
+                    {task.description}
+                  </div>
                 </div>
               ))
-
             )}
           </div>
         )}
       </div>
     </>
-  )
+  );
+};
 
-}
-
-export default Tasks
+export default Tasks;
