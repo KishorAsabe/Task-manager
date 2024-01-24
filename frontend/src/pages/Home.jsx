0e674c97429a94ee284f25c884ff5a6a1ec30f89
KishorAsabe <kishorasabe2020@gmail.com>
2024-01-24 09:35:07 +0530

    update styling Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,40 +1,55 @@
-import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import Tasks from '../components/Tasks';
-import MainLayout from '../layouts/MainLayout';
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import Tasks from "../components/Tasks";
+import MainLayout from "../layouts/MainLayout";
+import task from "../assets/task.jpg";
 
 const Home = () => {
-
-  const authState = useSelector(state => state.authReducer);
+  const authState = useSelector((state) => state.authReducer);
   const { isLoggedIn } = authState;
 
   useEffect(() => {
-    document.title = authState.isLoggedIn ? `${authState.user.name}'s tasks` : "Task Manager";
+    document.title = isLoggedIn
+      ? `${authState.user.name}'s tasks`
+      : "Task Manager";
   }, [authState]);
-
-
 
   return (
     <>
       <MainLayout>
         {!isLoggedIn ? (
-          <div className='bg-primary text-white h-[40vh] py-8 text-center'>
-            <h1 className='text-2xl'> Welcome to Task Manager App</h1>
-            <Link to="/signup" className='mt-10 text-xl block space-x-2 hover:space-x-4'>
-              <span className='transition-[margin]'>Join now to manage your tasks</span>
-              <span className='relative ml-4 text-base transition-[margin]'><i className="fa-solid fa-arrow-right"></i></span>
-            </Link>
+          <div className="flex flex-col items-center justify-center h-screen bg-richblack-800 text-white relative ">
+          <div className="absolute inset-0 bg-black opacity-50 "></div>
+
+            <div className="absolute inset-0 flex flex-col items-center justify-center mt-[-150px]">
+
+              <h1 className="text-4xl font-bold mb-4 text-center">
+                Welcome to Task Manager App
+              </h1>
+              <p className="text-lg mb-8 text-center max-w-md">
+                Organize your tasks efficiently with Task Manager.
+              </p>
+              <Link
+                to="/signup"
+                className="bg-richblack-50 text-blue-500 px-6 py-3 rounded-full text-lg font-semibold hover:bg-richblack-white hover:text-black transition-all"
+              >
+                Join now to manage your tasks{" "}
+                <i className="fas fa-arrow-right ml-2"></i>
+              </Link>
+            </div>
           </div>
         ) : (
           <>
-            <h1 className='text-lg mt-8 mx-8 border-b border-b-gray-300'>Welcome {authState.user.name}</h1>
+            <h1 className="text-2xl font-bold mt-8 mb-4 text-white text-center">
+              Welcome, {authState.user.name}!
+            </h1>
             <Tasks />
           </>
         )}
       </MainLayout>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
