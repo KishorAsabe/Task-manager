203c64962a1281ed8b2a7b4b5d04a74592b005b0
KishorAsabe <kishorasabe2020@gmail.com>
2024-01-24 11:16:37 +0530

    update NotFound.jsx

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -4,9 +4,9 @@ import MainLayout from '../layouts/MainLayout'
 const NotFound = () => {
   return (
     <MainLayout>
-      <div className='w-full py-16 text-center'>
-        <h1 className='text-7xl my-8'>404</h1>
-        <h2 className='text-xl'>The page you are looking for doesn't exist</h2>
+      <div className='w-full py-16 text-center text-wh'>
+        <h1 className='text-7xl my-8 text-white'>404</h1>
+        <h2 className='text-xl text-white'>The page you are looking for doesn't exist</h2>
       </div>
     </MainLayout>
   )
